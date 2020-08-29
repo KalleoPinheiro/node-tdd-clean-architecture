baa97151a3a1dc668c1e068002d08b57f4c3f079
Kalléo Pinheiro <kalleopinheiro@hotmail.com>
2020-08-29 14:58:56 -0300

    test: ensure SignUpController return 400 if no name is provided

--- src/sum.spec.ts
+++
@@ -1,9 +0,0 @@
-import { sum } from './sum'
-
-describe('Sum 2 numbers', () => {
-  it('should return sum of two numbers', () => {
-    const result = sum(1, 3)
-
-    expect(result).toBe(4)
-  })
-})
