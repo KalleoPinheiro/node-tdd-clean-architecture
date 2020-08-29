3f17b503994e554870be073eaa4f3ccfea33229b
Kalléo Pinheiro <kalleopinheiro@hotmail.com>
2020-08-29 15:54:13 -0300

    feat: ensure SignUpController returns 400 if password and passwordConfirmation does not match

--- src/Presentation/controllers/signup.ts
+++ src/Presentation/controllers/signup.ts
@@ -5,10 +5,18 @@ import { badRequest } from '../helpers/http-helper'
 export class SignUpController {
   handle(httpRequest: HttpRequest): HttpResponse {
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
+    const { password, passwordConfirmation } = httpRequest.body
 
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
         return badRequest(new MissingParamError(field))
+      }
+    }
+
+    if (password !== passwordConfirmation) {
+      return {
+        statusCode: 400,
+        body: new Error('Password does not match')
       }
     }
   }
